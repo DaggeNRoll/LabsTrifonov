51cd650b37a912698a49067490335cb5053ab76f
DaggeNRoll <71384493+DaggeNRoll@users.noreply.github.com>
2020-09-20 17:14:07 +0300

    Task 3 complete!

---
+++ Theme4/Task3/Task3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+using namespace std;
+int main() {
+	float a, b;
+	cout << "a=";
+	cin >> a;
+	cout << "b=";
+	cin >> b;
+	cout << "the ariphmetic mean is " << (a+b)/2;
+	return 0;
+}

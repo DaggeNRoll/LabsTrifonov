5d452ae8bf82dbfcb0fbf1e645a96dc5193ffe24
DaggeNRoll <71384493+DaggeNRoll@users.noreply.github.com>
2020-09-20 16:20:37 +0300

    Task1 complete!

--- Theme4/Task1/Task1.cpp
+++ Theme4/Task1/Task1.cpp
@@ -1,1 +1,19 @@
-öóööóöóöó
+#include <iostream>;
+using namespace std;
+float P(float a, float b) {
+	return 2 * (a + b);
+}
+float S(float a, float b) {
+	return a * b;
+}
+int main() {
+	float a, b;
+	cout << "Enter unparallel sides of the rectangle" << endl;
+	cout <<"a="; 
+	cin >> a;
+	cout << "b=";
+	cin >> b;
+	cout << "P=" << P(a, b) << endl;
+	cout << "S=" << S(a, b) << endl;
+	return 0;
+}

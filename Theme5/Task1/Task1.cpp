60cb519c62c5862a0c55ed6dd898af658f1e446d
DaggeNRoll <71384493+DaggeNRoll@users.noreply.github.com>
2020-09-24 12:34:15 +0300

    Task1

---
+++ Theme5/Task1/Task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>, <cmath>;
+using namespace std;
+void main() {
+	float x1, y1, x2, y2;
+	cout << "x1=";
+	cin >> x1;
+	cout << "y1=";
+	cin >> y1;
+	cout << "x2=";
+	cin >> x2;
+	cout << "y2=";
+	cin >> y2;
+	cout << "R=" << sqrtf((x2 - x1) * (x2 - x1) + (y1 - y2) * (y1 - y2));
+}
